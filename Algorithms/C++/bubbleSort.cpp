05f46e865f1a1c41117846341a831ef40140c2d4
hkrobotics <kumarhemant24jan@gmail.com>
2022-10-08 02:16:31 +0530

    ⬆️ add bubble sort

---
+++ Algorithms/C++/bubbleSort.cpp
@@ -0,0 +1,33 @@
+// bubble sort in c++
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
